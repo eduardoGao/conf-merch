a68923dbf33fb23c44ff8b41559e8afe1a601b54
Lalo Diaz <lalo.diaz1896@gmail.com>
2020-11-26 14:41:27 -0600

    Arquitectura con Router terminada

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import Home from '../containers/Home'
-import Checkout from '../containers/Checkout'
+import { Home, Checkout, Information, Payment, Success, NotFound } from '../containers/index'
 
 function App() {
   return (
@@ -10,6 +9,10 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/checkout' component={Checkout} />
+        <Route exact path='/checkout/information' component={Information} />
+        <Route exact path='/checkout/payment' component={Payment} />
+        <Route exact path='/checkout/success' component={Success} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   )
